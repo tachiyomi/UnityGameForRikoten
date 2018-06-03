613868c0e94c60475534e45ea9d2af8a80f253cb
立ち読み <naonosuke311@yahoo.co.jp>
2018-06-03 11:44:04 +0900

    [add]移動アニメーションを追加。
    移動アニメーションを追加。ジャンプは未完成。

--- Stage(kari)/Assets/CharacterController.cs
+++ Stage(kari)/Assets/CharacterController.cs
@@ -5,36 +5,46 @@ using UnityEngine;
 public class CharacterController : MonoBehaviour {
 
     Rigidbody rb;
+    Animator anim;
 
 	// Use this for initialization
 	void Start () {
         rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-        if (Input.GetKey(KeyCode.W)) {
-            rb.velocity = new Vector3(0, 0, 2f);
+	void Update ()
+    {
+        anim.SetFloat("Velocity", rb.velocity.z);
+        anim.SetFloat("Floating", rb.velocity.y);
+
+        if (Input.GetKey(KeyCode.W) && rb.velocity.y == 0) {
+            if (Input.GetKey(KeyCode.LeftShift))
+                rb.velocity = new Vector3(0, 0, 4f);
+            else
+                rb.velocity = new Vector3(0, 0, 2f);
         }
 
-        if (Input.GetKey(KeyCode.S))
+        if (Input.GetKey(KeyCode.S) && rb.velocity.y == 0)
         {
-            rb.velocity = new Vector3(0, 0, -2f);
+            rb.velocity = new Vector3(0, 0, -1.5f);
         }
 
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.D) && rb.velocity.y == 0)
         {
             rb.velocity = new Vector3(2f, 0, 0);
         }
 
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.A) && rb.velocity.y == 0)
         {
             rb.velocity = new Vector3(-2f, 0, 0);
         }
 
-        if(Input.GetKey(KeyCode.Space))
+        if (Input.GetKey(KeyCode.Space) && rb.velocity.y == 0)
         {
-            rb.AddForce(new Vector3(0, 50f, 0));
+            rb.velocity = new Vector3(0, 10f, 0);
+            anim.SetTrigger("Jump!");
         }
     }
 }
